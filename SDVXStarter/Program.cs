f8bb3d3c25e1144646564c07bb22007d1c29ee2d
Neskol <hs-neskol@outlook.com>
2021-05-03 16:07:09 +0800

    Changed Form

--- SDVXStarter/Program.cs
+++ SDVXStarter/Program.cs
@@ -17,7 +17,7 @@ namespace SDVXStarter
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new SDVX());
         }
     }
 }
